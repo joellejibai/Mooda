22023f6049c97ee91cb2f8f2c3191358703320c8
DESKTOP-MC0EKR2\User <nourmghalayini@hotmail.com>
2025-04-02 21:49:45 +0300

    hei

--- frontend/frontend/src/components/Navbar.js
+++ frontend/frontend/src/components/Navbar.js
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useAuthPages } from "../hooks/useAuthPages";
 
-
-
 const Navbar = () => {
     const { logout } = useLogout();
-    const { user} =useAuthPages()
+    const { user } = useAuthPages();
     const handleClick = () => {
         logout();
     };
@@ -21,42 +19,113 @@ const Navbar = () => {
                     <img src="/logo.png" alt="ModaNova Logo" style={{ width: "130px", height: "auto" }} />
                 </Link>
 
-              
-
                 <nav>
-                  
-
-                 
-
                     {/* Contact Us with Hover Dropdown */}
-                    <div 
+                    <div
                         className="dropdown"
                         onMouseEnter={() => setIsDropdownOpen(true)}
                         onMouseLeave={() => setIsDropdownOpen(false)}
                     >
-                        <Link to="/" className="dropdown-btn">Wardrobe</Link>
+                        <Link to="/" className="dropdown-btn" style={{ display: "flex", alignItems: "center" }}>
+                            <img
+                                src="/wardrobe.png"
+                                alt="Wardrobe Icon"
+                                style={{ width: "20px", height: "20px", marginRight: "8px" }}
+                            />
+                            Wardrobe
+                        </Link>
+
                         {isDropdownOpen && (
                             <ul className="dropdown-menu">
-                                <li><Link to="/AboutUs">About Us</Link></li>
-                                <li><Link to="/contactUs">Contact Us</Link></li>
-                                <li><Link to="/fashiontips">Fashion Tips</Link></li>
+                                <li>
+                                    <Link to="/AboutUs">
+                                        <img
+                                            src="/aboutus.png"
+                                            alt="About Icon"
+                                            style={{ width: "30px", height: "20px", marginRight: "5px" }}
+                                        />
+                                        About Us
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/contactUs">
+                                        <img
+                                            src="/contactus.png"
+                                            alt="Contact Icon"
+                                            style={{ width: "30px", height: "20px", marginRight: "5px" }}
+                                        />
+                                        Contact Us
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/fashiontips">
+                                        <img
+                                            src="/tips.png"
+                                            alt="Fashion Tips Icon"
+                                            style={{ width: "30px", height: "20px", marginRight: "5px" }}
+                                        />
+                                        Fashion Tips
+                                    </Link>
+                                </li>
                             </ul>
                         )}
                     </div>
+
                     {user && (
-  <div className="nav-buttons">
-    
-    <Link to="/dressUp">DressUp</Link>
-    <button onClick={handleClick}>Logout</button>
-  </div>
-)}
+                        <div className="nav-buttons" style={{ display: "flex", gap: "10px" }}>
+                            <Link to="/dressUp" style={{ display: "flex", alignItems: "center", gap: "5px" }}>
+                                <img
+                                    src="/dressup.png"
+                                    alt="DressUp Icon"
+                                    style={{
+                                        width: "40px",
+                                        height: "30px",
+                                        marginRight: "5px",
+                                        verticalAlign: "middle",
+                                    }}
+                                />
+                                DressUp
+                            </Link>
+                            <button
+                                onClick={handleClick}
+                                style={{ display: "flex", alignItems: "center", gap: "5px" }}
+                            >
+                                <img
+                                    src="/logout.png"
+                                    alt="Logout Icon"
+                                    style={{ width: "50px", height: "20px" }}
+                                />
+                                Logout
+                            </button>
+                        </div>
+                    )}
 
+                    {!user && (
+                        <div style={{ display: "flex", gap: "10px" }}>
+                            <Link
+                                to="/login"
+                                style={{ display: "flex", alignItems: "center", gap: "5px" }}
+                            >
+                                <img
+                                    src="/login.png"
+                                    alt="Login Icon"
+                                    style={{ width: "20px", height: "20px" }}
+                                />
+                                Login
+                            </Link>
 
-                    {!user &&(
-                        <div>
-                        <Link to="/login">Login</Link>
-                        <Link to="/signup">Sign Up</Link>
-                    </div>
+                            <Link
+                                to="/signup"
+                                style={{ display: "flex", alignItems: "center", gap: "5px" }}
+                            >
+                                <img
+                                    src="/signup.png"
+                                    alt="Sign Up Icon"
+                                    style={{ width: "20px", height: "20px" }}
+                                />
+                                Sign Up
+                            </Link>
+                        </div>
                     )}
                 </nav>
             </div>
